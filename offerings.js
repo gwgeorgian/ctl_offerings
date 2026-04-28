4d1abeb5958d493574e4b50a5bbcc8e3a9798887
gwgeorgian <grace.ward@georgiancollege.ca>
2026-04-28 12:39:28 -0400

    Update offerings.js

--- offerings.js
+++ offerings.js
@@ -1,1 +1,1 @@
-window.globalOfferings = ["Teaching @ Georgian", "Faculty Focus", "Blackboard Ultra", "Screenpal"];
+window.globalOfferings = ["Teaching @ Georgian", "Faculty Focus", "Blackboard Ultra", "Screenpal","Skoden","Key Insights"];
